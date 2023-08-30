c38d010cb8e01b0c161a67d7e282f9db1951f108
borraquim <119462593+borraquim@users.noreply.github.com>
2023-08-30 16:06:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  colwnNose = loadImage('https://postimg.cc/Xr5z2PKj/clownnose.png');
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses)
+}
+
+function modelLoaded() {
+console.log('PoseNet está inicializando')
+}
+
+function gotPoses(results);
+{
+ if(results.length > 0)
+ {
+  console.log(results);
+  noseX = results[0].pose.nose.x
+  noseY = results[0].pose.nose.y
+  console.log("nose x =" + results[0].pose.nose.x);
+  console.log("nose y =" + results[0].pose.nose.y);
+ }
+}
+
+function draw() {
+  image(video, 0 ,0 , 300, 300);
+  fill(255,0,0);
+  stroke(255,0,0);
+  circle(noseX, noseY, 20);
+
+}
+
+function takeSnapshot(){
+   save('myFilter.png');  
+  }
